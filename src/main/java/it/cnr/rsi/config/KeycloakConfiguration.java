4de0cd4d7cfc661502d077b21b210a19330c3e6b
marco.spasiano <marco.spasiano@cnr.it>
2021-12-22 12:32:52 +0100

    Add keycloak configuration

--- src/main/java/it/cnr/rsi/config/KeycloakConfiguration.java
+++ src/main/java/it/cnr/rsi/config/KeycloakConfiguration.java
@@ -1,14 +1,19 @@
 package it.cnr.rsi.config;
 
+import org.apache.commons.lang3.StringUtils;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,12 +21,22 @@ import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 @ConditionalOnProperty("keycloak.auth-server-url")
 @ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 public class KeycloakConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+    public static final String SAVED_LOGIN_ORIGIN_URI = KeycloakConfiguration.class.getName() + "_SAVED_ORIGIN";
+
+    private final Logger log = LoggerFactory.getLogger(KeycloakConfiguration.class);
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -43,10 +58,33 @@ public class KeycloakConfiguration extends KeycloakWebSecurityConfigurerAdapter 
             new SessionRegistryImpl());
     }
 
+    @Bean
+    public FilterRegistrationBean<OncePerRequestFilter> saveLoginOriginFilter() {
+        OncePerRequestFilter filter = new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain filterChain)
+                throws ServletException, IOException {
+                if (request.getRemoteUser() == null && request.getRequestURI().endsWith("/sso/login")) {
+                    String referrer = request.getHeader("referer");
+                    if (!StringUtils.isBlank(referrer) &&
+                        request.getSession().getAttribute(SAVED_LOGIN_ORIGIN_URI) == null) {
+                        log.debug("Saving login origin URI: {}", referrer);
+                        request.getSession().setAttribute(SAVED_LOGIN_ORIGIN_URI, referrer);
+                    }
+                }
+                filterChain.doFilter(request, response);
+            }
+        };
+        FilterRegistrationBean<OncePerRequestFilter> bean = new FilterRegistrationBean<>(filter);
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeRequests()
-            .antMatchers("/**").authenticated();
+            .antMatchers("/**").fullyAuthenticated();
     }
 }
