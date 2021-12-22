997cc5a6a60a1223a1a0ab4885a6ddaea1a43d50
marco.spasiano <marco.spasiano@cnr.it>
2021-12-22 12:16:15 +0100

    Add keycloak configuration

--- src/main/java/it/cnr/rsi/config/WebConfigurer.java
+++ src/main/java/it/cnr/rsi/config/WebConfigurer.java
@@ -27,6 +27,9 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -64,6 +67,22 @@ public class WebConfigurer implements ServletContextInitializer {
                 ((TomcatServletWebServerFactory) server).addAdditionalTomcatConnectors(redirectConnector());
             }
         };
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     private Connector redirectConnector() {
