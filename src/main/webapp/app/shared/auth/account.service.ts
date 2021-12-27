a7cd4c6cccf5f0bb1d83531607d4825bde1aac96
marco.spasiano <marco.spasiano@cnr.it>
2021-12-27 11:37:24 +0100

    Add keycloak configuration

--- src/main/webapp/app/shared/auth/account.service.ts
+++ src/main/webapp/app/shared/auth/account.service.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { SERVER_API_URL } from '../../app.constants';
+import { Headers } from '@angular/http';
 
 @Injectable()
 export class AccountService  {
     constructor(private http: Http) { }
+    headers: Headers = new Headers ({
+        'Access-Control-Allow-Origin': '*'
+    });
 
     get(user?: string): Observable<any> {
         let url = 'api/account';
         if (user) {
             url += '/' + user;
         }
-        return this.http.get(url).map((res: Response) => res.json());
+        return this.http.get(url, { headers: this.headers}).map((res: Response) => res.json());
     }
 
     save(account: any): Observable<Response> {
